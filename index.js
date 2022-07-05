84e31b8515f591df4187e21f64a768bed0e7eac2
TGab99 <gwolf938@gmail.com>
2022-07-05 23:02:47 +0200

    Add onclick function to images and close and window.

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-const images = document.querySelectorAll("img");
+let images = document.getElementsByClassName("pic");
+let show_image = document.getElementById("show-image");
+let image_content = document.getElementById("image-content");
+let close = document.getElementById("close");
 
-images.forEach((img) => {
-    img.addEventListener("click", () => {
-        img.style.transform = "scale(1.5)";
-        img.style.transition = "transform 0.25s ease";
-    });
-});
+for(let i = 0; i < images.length; i++) {
+    images[i].onclick = function() {
+        show_image.style.display = "block";
+        image_content.src = this.src;
+    }
+}
+
+close.onclick = function() {
+    show_image.style.display = "none";
+}
+
+window.onclick = function(e) {
+    if(e.target == show_image) {
+        show_image.style.display = "none";
+    }
+}
